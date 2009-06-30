d1a41eb79b5603159763c294cc4c75b2bd6b37d6
Christian Rishøj <christian@rishoj.net>
2009-06-30 12:02:17 +0200

    clause annotator

---
+++ lib/annotation/clause.rb
@@ -0,0 +1,22 @@
+# encoding: UTF-8
+
+module Annotation
+  class Clause < Annotator
+    BOUNDARIES = %w{, ; : - –}
+    
+    def pre_sentence
+      @clause = 1
+    end
+
+    def mark_token
+      if BOUNDARIES.include? @token.form
+        # Count clause boundary
+        @clause += 1 
+      else
+        # Mark clause number
+        @token.features << "clause=#{@clause}"
+      end
+    end
+
+  end
+end
