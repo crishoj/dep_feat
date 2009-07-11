54e5b6b28d90a4e73b79f5e85167036a7d2519dc
Christian Rishøj <christian@rishoj.net>
2009-07-11 15:06:13 +0200

    add QuotationMark

---
+++ lib/annotation/quotation_mark.rb
@@ -0,0 +1,27 @@
+
+module Annotation
+
+  class QuotationMark < Annotator
+
+    def mark_token
+      if @token.form == '"'
+        @token.pos = 'XQ'
+        @token.cpos = 'XQ'
+      end
+    end
+
+    def sentence_affected?
+      @sentence.tokens.find { |tok| tok.pos == 'XQ' or tok.cpos == 'XQ'}
+    end
+
+    def categorize_sentence
+      if sentence_affected?
+        "has-quotes"
+      else
+        "no-quotes"
+      end
+    end
+
+  end
+
+end
