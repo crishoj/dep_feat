6b9538f4f3ece0eb26da1ebe26aa20e2331a02df
Christian Rishøj <christian@rishoj.net>
2009-07-13 16:51:20 +0200

    expand list items

--- lib/annotation/bullet.rb
+++ lib/annotation/bullet.rb
@@ -3,19 +3,20 @@ module Annotation
   class Bullet < Annotator
 
     LIST_COUNTER  = /^[0-9]+$/ # A numeral 
-    BULLET_MARKER = /^[).]$/   # A closing parenthesis or a dot
+    BULLET_MARKER = /^[).:]$/  # A closing parenthesis, a dot or a colon
 
     def feature
       'bullet'
     end
 
     def pre_sentence
-      if @sentence.tokens[0].form =~ LIST_COUNTER
+      return if @sentence.tokens.size < 2
+#      if @sentence.tokens[0].form =~ LIST_COUNTER
         if @sentence.tokens[1].form =~ BULLET_MARKER
           @sentence.tokens[0].features << feature
           @sentence.tokens[1].features << feature
         end
-      end
+#      end
     end
 
   end
