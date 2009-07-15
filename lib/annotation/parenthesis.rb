65bca47ffa549f42eb1297c29830000f39e6bbf5
Christian Rishøj <christian@rishoj.net>
2009-07-15 15:33:22 +0200

    fix

--- lib/annotation/parenthesis.rb
+++ lib/annotation/parenthesis.rb
@@ -1,4 +1,6 @@
 
+# TODO: Match parentheticals with commas (tokens inside < 5, or train chunker?)
+
 module Annotation
   class Parenthesis < Span
 
@@ -11,13 +13,23 @@ module Annotation
     end
 
     def mark_token
-      if @token.form == '('
-        @in_parenthesis = true
-      elsif @token.form == ')'
+      if @in_parenthesis and closing?
         @in_parenthesis = false
+      elsif opening?
+        @in_parenthesis = true
       else
         @token.features << feature if @in_parenthesis
       end
+    end
+
+    protected
+
+    def opening?
+      @token.form == '('
+    end
+    
+    def closing?
+      @token.form == ')'
     end
 
   end
