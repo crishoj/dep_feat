8fdc2b649c964d5746b1cda75352067ad54a3121
Christian Rishøj <christian@rishoj.net>
2009-07-24 18:01:15 +0200

    use POS tag for lemmatising. fix alignment in lemmatising.

--- lib/annotation.rb
+++ lib/annotation.rb
@@ -1,4 +1,8 @@
 
 module Annotation
-    
+
+  def self.find_annotator(kind)
+    Annotation.const_get(kind.camelize)
+  end
+
 end
---
+++ lib/annotation/lemma.rb
@@ -0,0 +1,38 @@
+
+module Annotation
+  class Lemma < Base
+
+    def initialize
+      @bin       = "vendor/cstlemma/bin/cstlemma"
+      @base      = 'models/lemma'
+      @flex_file = "#{@base}/flexrules.UTF8"
+      @dict_file = "#{@base}/dict.UTF8"
+      @translation_file = "#{@base}/translation.DDT"
+      @friends_file = "#{@base}/friends.UTF8"
+    end
+
+    def pre_corpus
+      @input_file =  '/tmp/lemmatiser-input.txt'
+      @output_file = '/tmp/lemmatiser-output.txt'
+      @lemmas = []
+      File.open(@input_file, 'w') do |f|
+        for sent in @corpus.sentences
+          for token in sent.tokens
+            f.puts "#{token.form}/#{token.pos}"
+          end
+        end
+      end
+      `#{@bin} -L -f #{@flex_file} -d #{@dict_file} -x #{@translation_file} -v #{@friends_file} -i #{@input_file} -o #{@output_file}`
+      File.open(@output_file) do |f|
+        while (line = f.gets)
+          @lemmas.push line.split(/\t/)[1]
+        end
+      end
+    end
+
+    def mark_token
+      @token.lemma = @lemmas.shift
+    end
+
+  end
+end
