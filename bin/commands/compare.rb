8d25f198290bb0d58e9edd4e2f1eb0b06ed9bf17
Christian Rishøj <christian@rishoj.net>
2009-07-26 22:51:32 +0200

    optionally ignore punctuation

--- bin/commands/compare.rb
+++ bin/commands/compare.rb
@@ -2,10 +2,12 @@
 command :compare do |c|
   c.syntax = 'corpus compare DIR CATEGORY'
   c.description = 'Use the CoNLL eval script to compare a baseline against the system output, in order to determine the effect of the introduced feature.'
+  c.option '-p', 'Do not score punctuation'
   c.when_called do |args, options|
     if args.size < 1
       say "Missing corpus directory"
     else
+      punc = options.p ? '-p': ''
       dir = args[0]
       cat = args[1] || ''
       gold = Dir.glob("#{dir}/test/#{cat}/*.conll").first
@@ -16,12 +18,12 @@ command :compare do |c|
       evalbs = []
       for file in contenders
         say "[eval] #{file}"
-        puts `bin/eval.pl -q -g #{gold} -s #{file} 2> /dev/null`
+        puts `bin/eval.pl #{punc} -q -g #{gold} -s #{file} 2> /dev/null`
       end
       evalbs = contenders.collect { |file|
         evalb = File.dirname(file) + "/evalb.out"
         say "[evalb] #{file} => #{evalb}"
-        `bin/eval.pl -b -q -g #{gold} -s #{file} > #{evalb} 2> /dev/null`
+        `bin/eval07.pl #{punc} -b -q -g #{gold} -s #{file} > #{evalb} 2> /dev/null`
         evalb
       }
       say "[compare] #{evalbs[0]} #{evalbs[1]}"
--- bin/commands/eval.rb
+++ bin/commands/eval.rb
@@ -2,9 +2,11 @@
 command :eval do |c|
   c.syntax = 'corpus eval DIR'
   c.description = 'Run the CoNLL eval script on the given corpus'
+  c.option '-p', 'Do not score punctuation'
   c.when_called do |args, options|
     system = Dir.glob("#{args[0]}/system/*.conll").first
     gold   = Dir.glob("#{args[0]}/test/*.conll").first
-    say `bin/eval.pl -g #{gold} -s #{system}`
+    punc = options.p ? '-p': ''
+    say `bin/eval07.pl #{punc} -g #{gold} -s #{system}`
   end
 end
