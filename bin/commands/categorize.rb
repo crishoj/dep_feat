5ed21921d5850cbfee0a1be6f566024f4b383a41
Christian Rishøj <christian@rishoj.net>
2009-07-26 22:51:20 +0200

    reflect change in API method signature

--- bin/commands/categorize.rb
+++ bin/commands/categorize.rb
@@ -1,7 +1,7 @@
 
 command :categorize do |c|
   c.syntax = 'corpus categorize --kind ANNOTATOR DIR'
-  c.description = 'Split corpus into several parts according to the presence of the feature introduced by the specified annotator. This is useful for evaluating the effect of introducing the feature.'
+  c.description = 'Split corpus into several parts according to the presence of a feature. This is useful for evaluating the effect of introducing the feature.'
   c.when_called do |args, options|
     if not options.kind
       say "Missing annotator class"
@@ -14,12 +14,12 @@ command :categorize do |c|
       gold_file = Dir.glob("#{dir}/test/*.conll").first
       say "      [gold] #{gold_file}"
       gold = Conll::Corpus.parse gold_file
-      annotator = Annotation::find_annotator(options.kind).new(gold)
+      annotator = Annotation::find_annotator(options.kind).new
       %w{baseline test system}.each do |section|
         filename = Dir.glob("#{dir}/#{section}/*.conll").first
         corpus = Conll::Corpus.parse filename
         say "[categorize] #{filename}"
-        annotator.categorize(corpus).each_pair do |category, part|
+        annotator.categorize(gold, corpus).each_pair do |category, part|
           category_filename = filename.sub(/#{section}/, "#{section}/#{category}")
           make_directory File.dirname(category_filename)
           say "      [part] #{category} => #{category_filename}"
