b255633b322ad3c2570f421a7763fc8d5684fafc
Christian Rishøj <christian@rishoj.net>
2009-08-02 14:15:32 +0200

    allow batch

--- bin/commands/annotate.rb
+++ bin/commands/annotate.rb
@@ -9,35 +9,38 @@ command :annotate do |c|
     designation = kinds.join('+')
     if args.size == 0
       say "Missing corpus directory"
-    elsif File.directory?(args[0])
-      dir = args[0]
-      target_dir = "#{dir}-#{designation}"
-      make_directory(target_dir)
-      Dir.glob("#{dir}/*/*.conll").each do |source|
-        target = source.gsub(dir, target_dir)
-        target_subdir = File.dirname(target)
-        if target_subdir =~ /baseline$/
-          next
-        elsif target_subdir =~ /system$/
-          # Annotate a copy of the baseline system output (for evaluation)
-          say "  [baseline] #{source}"
-          target_subdir.gsub! /system/, 'baseline'
-          target.gsub!        /system/, 'baseline'
-        end
-        make_directory(target_subdir)
-        corpus = Conll::Corpus.parse(source)
-        kinds.each do |kind|
-          puts "  [annotate] #{source} (#{kind})"
-          annotator = Annotation::find_annotator(kind).new
-          corpus = annotator.annotate(corpus)
-        end
-        File.open(target, 'w') do |f|
-          puts "      [save] #{source} => #{target}"
-          f << corpus
+    else
+      for dir in args
+        if File.directory?(dir)
+          target_dir = "#{dir}-#{designation}"
+          make_directory(target_dir)
+          Dir.glob("#{dir}/*/*.conll").each do |source|
+            target = source.gsub(dir, target_dir)
+            target_subdir = File.dirname(target)
+            if target_subdir =~ /baseline$/
+              next
+            elsif target_subdir =~ /system$/
+              # Annotate a copy of the baseline system output (for evaluation)
+              say "  [baseline] #{source}"
+              target_subdir.gsub! /system/, 'baseline'
+              target.gsub!        /system/, 'baseline'
+            end
+            make_directory(target_subdir)
+            corpus = Conll::Corpus.parse(source)
+            kinds.each do |kind|
+              puts "  [annotate] #{source} (#{kind})"
+              annotator = Annotation::find_annotator(kind).new
+              corpus = annotator.annotate(corpus)
+            end
+            File.open(target, 'w') do |f|
+              puts "      [save] #{source} => #{target}"
+              f << corpus
+            end
+          end
+        else
+          say "Invalid corpus directory: #{dir}"
         end
       end
-    else
-      say "Invalid corpus directory"
     end
   end
 end
